b44bf6eebd0d98fcf166bafde4abea8276f9397a
ciurkoj <ciurkoj@outlook.com>
2022-12-20 17:30:47 +0100

    Added remote data source and its tests

--- lib/features/invoice/data/data_sources/invoice_remote_data_source.dart
+++ lib/features/invoice/data/data_sources/invoice_remote_data_source.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:invoices_tdd/core/error/exceptions.dart';
+import 'package:invoices_tdd/core/util/constants.dart';
 import 'package:invoices_tdd/features/invoice/data/models/invoice_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class InvoiceRemoteDataSource{
   /// Calls the  {...} endpoint
@@ -12,4 +15,42 @@ abstract class InvoiceRemoteDataSource{
   ///
   /// Throws a [ServerException] for all error codes
   Future<List<InvoiceModel>> getAllInvoices();
+}
+
+
+class InvoiceRemoteDataSourceImpl implements InvoiceRemoteDataSource {
+  late final http.Client client;
+
+  InvoiceRemoteDataSourceImpl({required this.client});
+
+  Future<InvoiceModel> _getInvoiceFromUrl(String url) async {
+    final response = await client
+        .get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
+    if (response.statusCode == 200) {
+      return const InvoiceModel(invoiceId: '1',vat:1);
+    } else {
+      throw ServerException();
+    }
+  }
+  Future<List<InvoiceModel>> _getAllInvoicesFromUrl(String url) async {
+    final response = await client
+        .get(Uri.parse(url), headers: {'Content-Type': 'application/json'});
+    if (response.statusCode == 200) {
+      return [const InvoiceModel(invoiceId: '1',vat:1),const InvoiceModel(invoiceId: '1',vat:1)];
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<InvoiceModel>> getAllInvoices() {
+    // TODO: implement getAllInvoices with the correct Url
+    return _getAllInvoicesFromUrl('${Constants.URL}/random');
+  }
+
+  @override
+  Future<InvoiceModel> getConcreteInvoice(String invoiceId) {
+    // TODO: implement getConcreteInvoice with the correct Url
+    return _getInvoiceFromUrl('${Constants.URL}/$invoiceId');
+  }
 }
---
+++ test/features/invoice/data/datasources/invoice_remote_data_source_test.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+
+import 'package:invoices_tdd/core/error/exceptions.dart';
+import 'package:invoices_tdd/core/util/constants.dart';
+import 'package:invoices_tdd/features/invoice/data/data_sources/invoice_remote_data_source.dart';
+import 'package:invoices_tdd/features/invoice/data/models/invoice_model.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture_reader.dart';
+import 'invoice_remote_data_source_test.mocks.dart';
+
+@GenerateMocks([InvoiceRemoteDataSourceImpl, http.Client])
+void main() {
+  late InvoiceRemoteDataSourceImpl dataSource;
+  late MockClient mockClient;
+
+  setUp(() {
+    mockClient = MockClient();
+    dataSource = InvoiceRemoteDataSourceImpl(client: mockClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    when(mockClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response(fixture('invoice.json'), 200));
+  }
+  void setUpMockHttpClientFailure404() {
+    when(mockClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response("Something went wrong", 404));
+  }
+
+  group('getConcreteInvoice', () {
+    const tInvoiceId = "1";
+    final tInvoiceModel = InvoiceModel.fromJson(json.decode(fixture('invoice.json')));
+
+    test('should perform a GET request on a URL with invoice id being the endpoint with application/json header',
+        () async {
+      //arrange
+      setUpMockHttpClientSuccess200();
+      //act
+      dataSource.getConcreteInvoice(tInvoiceId);
+      //assert
+      verify(mockClient.get(Uri.parse("${Constants.URL}/$tInvoiceId"), headers: {'Content-Type': 'application/json'}));
+    });
+
+    test(
+      'should return Imvoice when the response is 200 (success)',
+      () async {
+        // arrange
+        setUpMockHttpClientSuccess200();
+
+        // act
+        final result = await dataSource.getConcreteInvoice(tInvoiceId);
+        // assert
+        expect(result, equals(tInvoiceModel));
+      },
+    );
+
+    test(
+      'should throw a Server Exception when the response code is 404 or other',
+      () async {
+        // arrange
+        setUpMockHttpClientFailure404();
+        // act
+        final call = dataSource.getConcreteInvoice;
+        // assert
+        expect(call(tInvoiceId), throwsA(const TypeMatcher<ServerException>()));
+      },
+    );
+  });
+
+  group('getAllInvoices', () {
+    test('should perform a GET request on a URL to get all invoices being the endpoint with application/json header',
+        () async {
+      //arrange
+      when(mockClient.get(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response(fixture('all_invoices_cached.json'), 200));
+      //act
+      dataSource.getAllInvoices();
+      //assert
+      verify(mockClient.get(Uri.parse("${Constants.URL}/random"), headers: {'Content-Type': 'application/json'}));
+    });
+
+    test(
+      'should throw a Server Exception when the response code is 404 or other',
+          () async {
+        // arrange
+            when(mockClient.get(any, headers: anyNamed('headers')))
+                .thenAnswer((_) async => http.Response("Something went wrong", 404));
+        // act
+        final call = dataSource.getAllInvoices();
+        // assert
+        expect(call, throwsA(const TypeMatcher<ServerException>()));
+      },
+    );
+  });
+}
