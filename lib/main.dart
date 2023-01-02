95daabee1ed523af788c3ed17a9793e754cb40de
ciurkoj <ciurkoj@outlook.com>
2023-01-02 17:58:57 +0100

    Added FireBase Core to the project, updated bloc observer name and added debug condition for prints

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:invoices_tdd/bloc_observer.dart';
 import 'package:invoices_tdd/features/invoice/presentation/pages/invoices_page.dart';
 import 'injection_container.dart' as ic;
+import 'package:authentication_repository/authentication_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ic.init();
+  Bloc.observer = AppBlocObserver();
+
   runApp(const MyApp());
 }
 
