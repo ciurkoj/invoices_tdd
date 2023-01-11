1abf5ec62ac35ce628a39fdee2dc201092dcc35c
ciurkoj <ciurkoj@outlook.com>
2023-01-11 15:07:02 +0100

    Added Navigation Drawer

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -11,33 +11,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     final user = context.select((AppBloc bloc) => bloc.state.user);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            key: const Key('homePage_logout_iconButton'),
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () {
-              context.read<AppBloc>().add(const AppLogoutRequested());
-            },
-          )
-        ],
-      ),
-      // body:NumberTriviaPage()
-      body: ListView(
-        // mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Center(child: Avatar(photo: user.photo)),
-          const SizedBox(height: 4),
-          Text(user.email ?? '', style: textTheme.headlineSmall),
-          const SizedBox(height: 4),
-          Text(user.name ?? '', style: textTheme.headlineMedium),
-          InvoicesPage()
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        drawer: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: Drawer(
+            key: const Key('homePage_drawer'),
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent[700],
+                    ),
+                    accountName: Text(user.name ?? '',
+                        style: const TextStyle(color: Colors.white, fontSize: 24)),
+                    accountEmail: Text(user.email ?? '',
+                        style: const TextStyle(color: Colors.white, fontSize: 24)),
+                    currentAccountPicture: Center(child: Avatar(photo: user.photo)),
+                  ),
+                ),
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                        key: const Key('homePage_logout_iconButton'),
+                        onPressed: () => context.read<AppBloc>().add(const AppLogoutRequested()),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            Text(
+                              "Sign out",
+                              style: TextStyle(fontSize: 24),
+                            ),
+                            SizedBox(width: 6),
+                            Icon(Icons.exit_to_app)
+                          ],
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        // body:NumberTriviaPage()
+        body: Column(
+          children: [
+            Text("test"),
+            InvoicesPage(),
+          ],
+        ));
   }
 }
